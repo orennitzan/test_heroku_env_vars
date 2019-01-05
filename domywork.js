56d39512d3d6c9830edf493606389ced51484919
ALL_Nitzan-PC\ALL_Nitzan <oren.nitzan@gmail.com>
2019-01-05 18:33:43 +0200

    adding worker process

---
+++ domywork.js
@@ -0,0 +1,2 @@
+const d = new Date();
+console.log(`doing work ${d.toUTCString()}....`);
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 
-for (let i = 0; i < 10; i++) {
-    console.log('Working.. ' + i.toString());
+const express = require('express')
+const app = express()
+
+if (process.env.ENV && process.env.ENV !== 'prod-cloud') {
+    require('dotenv').config();
 }
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello Oren!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
